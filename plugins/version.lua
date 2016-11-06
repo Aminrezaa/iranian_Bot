7ae2ca8300e01c2e97bdf288b756785c23380f4c
Aminrezaa <aminrezaaslani1382@gmail.com>
2016-11-06 17:03:31 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -1,9 +1,8 @@
 do
 
 function run(msg, matches)
-  return 'Telegram Bot '.. VERSION .. [[ 
-  Checkout http://git.io/6jdjGg
-  GNU GPL v2 license.]]
+  return سلام!من ربات ضداسپم امین هستم
+    ورژن من 1 هست.
 end
 
 return {
